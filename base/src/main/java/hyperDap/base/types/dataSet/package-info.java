92d65eadb2ddbc029bf4340fe1f48d1cb4800010
Sonke <sonke.a.wohler@gmail.com>
2019-04-03 23:32:06 +0100

    added package documentation to base module

---
+++ base/src/main/java/hyperDap/base/types/dataSet/package-info.java
@@ -0,0 +1,9 @@
+
+/**
+ * This package contains {@link DataSet} and its subclasses. These are designed to hold information
+ * on collections of data that is to be accessed by an 'independent value' (also xValue). For
+ * illustration, measurements over time would be stored in a {@link ValueDataSet} where time is the
+ * independent value and the measured quantity (say temperature) is the dependent value (or yValue).
+ * Each temperature measurement is then stored relative to its corresponding time.
+ */
+package hyperDap.base.types.dataSet;
---
+++ base/src/main/java/hyperDap/base/types/package-info.java
@@ -0,0 +1,5 @@
+/**
+ * This package may be used throughout the project. It contains classes that store information i.e.
+ * data types.
+ */
+package hyperDap.base.types;
