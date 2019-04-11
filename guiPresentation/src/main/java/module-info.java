2246d114d0683ddfde85c345cf757d67eeeae036
Sonke <44615513+SonkeWohler@users.noreply.github.com>
2019-04-11 23:36:52 +0100

    created gui with inputs (#13)
    * created basic GUIMain without FX
    
    * added java fx to project and GUIMain
    
    * added javadoc to GUIMain
    
    * created first gui view
    
    * minor cleanup
    
    * implmented fxml loading UNTESTED
    
    * fixed fmlx loading
    
    * implemented Controller scaffolding
    
    * connected controller with GUIMain
    
    * implemented simple selection for data generation
    
    * added checks to inputs

---
+++ guiPresentation/src/main/java/module-info.java
@@ -0,0 +1,24 @@
+/**
+* 
+*/
+/**
+ * A module to demonstrate features of basic analyser implementations independently.
+ * <p>
+ * This is in particular used to present the course-work aspects of the software, but may be
+ * extended to any further features.
+ * 
+ * @author soenk
+ *
+ */
+module guiPresentation {
+  exports hyperDap.guiPres.application;
+  exports hyperDap.guiPres.views.honoursMainView;
+
+  requires javafx.base;
+  requires javafx.controls;
+  requires javafx.fxml;
+  requires transitive javafx.graphics;
+
+  opens hyperDap.guiPres.application to javafx.graphics, javafx.fxml;
+  opens hyperDap.guiPres.views.honoursMainView to javafx.graphics, javafx.fxml;
+}
