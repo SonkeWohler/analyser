89cc1b9fec8d040011a7c60fe5434cb249eea355
Sonke <sonke.a.wohler@gmail.com>
2019-04-17 13:22:00 +0100

    prepared for deployment

---
+++ guiPresentation/src/main/java/hyperDap/guiPres/application/Main.java
@@ -0,0 +1,15 @@
+package hyperDap.guiPres.application;
+
+/**
+ * The default Main from which the application should be run.
+ * 
+ * @author soenk
+ *
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    GUIMain.newGUIMain();
+  }
+
+}
